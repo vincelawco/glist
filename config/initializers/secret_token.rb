8d87eb3ffc7306f95a48762010d0fcd497b3c4ec
Vince Law <vincentslaw@gmail.com>
2013-11-07 19:03:55 -0500

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Glist::Application.config.secret_key_base = 'e9bbd9800e247148f548fdb57ff2502f3b13ef9e8d2c0183cb89810a5d5e80c56a077f1ecedf770fdb938ad180e5c90a8ef569d37781c21820c991e16bb7ec6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Glist::Application.config.session_store :cookie_store, key: '_glist_session'
